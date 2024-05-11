e8bac73ca0e86ea3974953279b11405ce576abbd
Oleg <oleh.s@seznam.cz>
2024-05-11 19:14:25 +0200

    Firebase

--- src/FairBase/firebase/config.js
+++ src/FairBase/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyDQMEqa9w8oOB-b8kKOiB0F3A8C2_hI_Ds',
-	authDomain: 'react-david.firebaseapp.com',
-	projectId: 'react-david',
-	storageBucket: 'react-david.appspot.com',
-	messagingSenderId: '733050178210',
-	appId: '1:733050178210:web:6d599aed547a2e671225fb',
+	apiKey: '...',
+	authDomain: '...',
+	projectId: '...',
+	storageBucket: '...',
+	messagingSenderId: '...',
+	appId: '...',
 }
 
 // počáteční nastavení firebase (init)
